125469d0e3dad57755dc61369bea1eed2cdb2801
romulocraveiro <romulocraveiro@gmail.com>
2022-02-08 11:29:49 -0300

    Solução com preventDefault

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ const generos = document.querySelector('#genero');
 generos.addEventListener('change', function (event) {
     console.log(event.target.value);
     q.value = event.target.value ? event.target.value : q.value;
-}); // Leitura da lógica: o input oculto com id 'q' tem valor igual ao input explícito (de id 'genero')? Se sim, considere o value apresentado e faça a pesquisa, abrindo seu resultado em outra janela. Se não, considere o value vazio. Nesse caso, q sem valor (= "", ou seja, string vazio) significa que o Google não fará a pesquisa.
+}); 
+
+// >>>>>>>>>>>>>>Outra forma de resolver:<<<<<<<<<<<<<<<<
+
+// const form = document.querySelector('form');
+// const q = document.querySelector('#q');
+// form.preventDefault('change', function (event) {
+//     console.log(event.target.value);
+//     q.value = event.target.value ? event.target.value : q.value;
+// });
 
 
-// const form = documento.querySelector('form');
-//     form.preventDefault();
+
+
