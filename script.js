c4717a845cf9d23f60500d3dca394de39bced8d5
Romulo Craveiro <romulocraveiro@gmail.com>
2022-05-17 21:00:36 -0300

    Refatoração

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const q = document.querySelector('#q');
-const botao = document.querySelector('.botao');
 const form = document.querySelector('form'); 
 const generos = document.querySelector('#genero');
 const modal = document.querySelector('modal');
 
-generos.addEventListener('change', function (event) {
-    
-    q.value = event.target.value ? event.target.value : q.value;
-   
+generos.addEventListener('change', event => {
+    q.value = event.target.value;
 });
 
 form.addEventListener('submit', (event)=>{
     
-    if(q.value ===""){
+    if(!q.value){
         event.preventDefault();
         modal.classList.remove('escondido');
-        return;
-    } else if (modal.classList ==='escondido'){
-        modal.classList.add('escondido');
-    }
-    
+        
+    } else {
+        modal.classList.add('escondido'); 
+    } 
+
 });
 
 
